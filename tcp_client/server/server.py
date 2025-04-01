e4ad93bddfe7648f920b8b01bbc92358e05fe325
Gizmo Head <gizmo.head@yahoo.de>
2025-04-01 15:43:21 +0000

    adds simple server

---
+++ tcp_client/server/server.py
@@ -0,0 +1,41 @@
+import socket
+import threading
+
+SIZE = 64 # how many symbols (bytes) to read
+PORT = 9000 # port number to listen on
+# SERVER = socket.gethostbyname(socket.gethostname()) # this gets the current IP addr.
+SERVER = '0.0.0.0'  # Listen on all interfaces
+ADDR = (SERVER, PORT)
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISCONNECT"
+
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server.bind(ADDR)
+
+def handle_client(conn, addr):
+    print(f"[NEW CONNECTION] {addr} connected.")
+
+    connected = True
+    while connected:
+        msg = conn.recv(SIZE).decode(FORMAT)
+        if msg:
+            print(f"[{addr}] {msg}")
+            conn.send("Msg received".encode(FORMAT))
+
+    conn.close()
+
+
+def start():
+    server.listen()
+    print(f"[LISTENING] Server is listening on {SERVER}")
+    while True:
+        conn, addr = server.accept()
+        thread = threading.Thread(target=handle_client, args=(conn, addr))
+        thread.start()
+        print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1}")
+
+
+print("[STARTING] server is starting...")
+start()
