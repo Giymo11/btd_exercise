f011a0cf90072163a9cc8e06b130d57ff2093c04
Gizmo Head <gizmo.head@yahoo.de>
2025-04-02 17:58:15 +0200

    implements task 4

--- tcp_client/main/tcp_client_v4.c
+++ tcp_client/main/tcp_client_v4.c
@@ -2,56 +2,53 @@
  * ESP32 TCP Client - Improved version with CONFIG_EXAMPLE_IPV4 assumption
  */
 
-#include <stdio.h>       // For snprintf
-#include <stdlib.h>      // Standard library functions (not strictly needed here anymore)
+#include <stdio.h>  // For snprintf
+#include <stdlib.h> // Standard library functions (not strictly needed here anymore)
 
 #include <string.h>
 #include <unistd.h>
 #include <sys/socket.h>
 #include <errno.h>
-#include <netdb.h>       // struct addrinfo, gai_strerror() if using DNS names
-#include <arpa/inet.h>   // inet_pton()
+#include <netdb.h>     // struct addrinfo, gai_strerror() if using DNS names
+#include <arpa/inet.h> // inet_pton()
 #include <driver/i2c.h>
 #include <math.h>
 
 #include "sdkconfig.h" // For Kconfig options
-#include "esp_netif.h"   // Networking Interface
-#include "esp_log.h"     // Logging framework
+#include "esp_netif.h" // Networking Interface
+#include "esp_log.h"   // Logging framework
 
 #include "freertos/FreeRTOS.h" // FreeRTOS API
 #include "freertos/task.h"     // Task management
-#include "esp_random.h"  // ESP32 Random number generator"
+#include "esp_random.h"        // ESP32 Random number generator"
 #include "esp_timer.h"
 
-
-// Configuration checks 
+// Configuration checks
 #if !defined(CONFIG_EXAMPLE_IPV4)
 #error "This application requires CONFIG_EXAMPLE_IPV4 to be defined"
 #endif
 
+#define I2C_MASTER_SCL_IO 22        /*!< GPIO number used for I2C master clock */
+#define I2C_MASTER_SDA_IO 21        /*!< GPIO number used for I2C master data  */
+#define I2C_MASTER_NUM 0            /*!< I2C master i2c port number */
+#define I2C_MASTER_FREQ_HZ 400000   /*!< I2C master clock frequency */
+#define I2C_MASTER_TX_BUF_DISABLE 0 /*!< I2C master doesn't need buffer */
+#define I2C_MASTER_RX_BUF_DISABLE 0 /*!< I2C master doesn't need buffer */
+#define I2C_MASTER_TIMEOUT_MS 1000
 
-#define I2C_MASTER_SCL_IO           22                         /*!< GPIO number used for I2C master clock */
-#define I2C_MASTER_SDA_IO           21                         /*!< GPIO number used for I2C master data  */
-#define I2C_MASTER_NUM              0                          /*!< I2C master i2c port number */
-#define I2C_MASTER_FREQ_HZ          400000                     /*!< I2C master clock frequency */
-#define I2C_MASTER_TX_BUF_DISABLE   0                          /*!< I2C master doesn't need buffer */
-#define I2C_MASTER_RX_BUF_DISABLE   0                          /*!< I2C master doesn't need buffer */
-#define I2C_MASTER_TIMEOUT_MS       1000
-
-#define MPU6886_SENSOR_ADDR                     0x68        /*!< Slave address of the MPU6866 sensor */
-#define MPU6886_WHO_AM_I_REG_ADDR               0x75        /*!< Register addresses of the "who am I" register */
-#define MPU6886_SMPLRT_DIV_REG_ADDR             0x19
-#define MPU6886_CONFIG_REG_ADDR                 0x1A
-#define MPU6886_ACCEL_CONFIG_REG_ADDR           0x1C
-#define MPU6886_ACCEL_CONFIG_2_REG_ADDR         0x1D
-#define MPU6886_FIFO_EN_REG_ADDR                0x23
-#define MPU6886_INT_PIN_CFG_REG_ADDR            0x37
-#define MPU6886_INT_ENABLE_REG_ADDR             0x38
-#define MPU6886_ACCEL_XOUT_REG_ADDR             0x3B
-#define MPU6886_USER_CRTL_REG_ADDR              0x6A
-#define MPU6886_PWR_MGMT_1_REG_ADDR             0x6B
-#define MPU6886_PWR_MGMT_2_REG_ADDR             0x6C
-
+#define MPU6886_SENSOR_ADDR 0x68       /*!< Slave address of the MPU6866 sensor */
+#define MPU6886_WHO_AM_I_REG_ADDR 0x75 /*!< Register addresses of the "who am I" register */
+#define MPU6886_SMPLRT_DIV_REG_ADDR 0x19
+#define MPU6886_CONFIG_REG_ADDR 0x1A
+#define MPU6886_ACCEL_CONFIG_REG_ADDR 0x1C
+#define MPU6886_ACCEL_CONFIG_2_REG_ADDR 0x1D
+#define MPU6886_FIFO_EN_REG_ADDR 0x23
+#define MPU6886_INT_PIN_CFG_REG_ADDR 0x37
+#define MPU6886_INT_ENABLE_REG_ADDR 0x38
+#define MPU6886_ACCEL_XOUT_REG_ADDR 0x3B
+#define MPU6886_USER_CRTL_REG_ADDR 0x6A
+#define MPU6886_PWR_MGMT_1_REG_ADDR 0x6B
+#define MPU6886_PWR_MGMT_2_REG_ADDR 0x6C
 
 #define HOST_IP_ADDR CONFIG_EXAMPLE_IPV4_ADDR
 #define PORT CONFIG_EXAMPLE_PORT
@@ -64,32 +61,28 @@ static const char *TAG = "TCP_CLIENT";
 static char rx_buffer[MAX_RX_SIZE];
 static char tx_buffer[MAX_TX_SIZE];
 
-
-typedef enum {
-    ORIENTATION_NEGATIVE_X = 0,
+typedef enum
+{
+    ORIENTATION_UNDEFINED = 0,
+    ORIENTATION_NEGATIVE_X,
     ORIENTATION_POSITIVE_X,
     ORIENTATION_NEGATIVE_Y,
     ORIENTATION_POSITIVE_Y,
     ORIENTATION_NEGATIVE_Z,
-    ORIENTATION_POSITIVE_Z,
-    ORIENTATION_UNDEFINED
+    ORIENTATION_POSITIVE_Z
 } device_orientation_t;
 
-static const char* orientation_names[] = {
+static const char *orientation_names[] = {
+    "undefined",
     "-X",
     "+X",
     "-Y",
     "+Y",
     "-Z",
-    "+Z",
-    "undefined"
-};
+    "+Z"};
 
-
-
-
-
-static int connect_to_server(const char *host_ip, int port) {
+static int connect_to_server(const char *host_ip, int port)
+{
     int sock = -1;
 
     int addr_family = AF_INET;
@@ -99,15 +92,17 @@ static int connect_to_server(const char *host_ip, int port) {
     memset(&dest_addr, 0, sizeof(dest_addr));
     dest_addr.sin_family = addr_family;
     dest_addr.sin_port = htons(port);
-    
+
     /* Convert IP address from string to binary form */
-    if (inet_pton(addr_family, host_ip, &dest_addr.sin_addr) != 1) {
+    if (inet_pton(addr_family, host_ip, &dest_addr.sin_addr) != 1)
+    {
         ESP_LOGE(TAG, "Invalid IP address format");
         return -1;
     }
-    
+
     sock = socket(addr_family, SOCK_STREAM, ip_protocol);
-    if (sock < 0) {
+    if (sock < 0)
+    {
         ESP_LOGE(TAG, "Unable to create socket: errno %d", errno);
         return -1;
     }
@@ -115,35 +110,44 @@ static int connect_to_server(const char *host_ip, int port) {
 
     /* Connect to server */
     int err = connect(sock, (struct sockaddr *)&dest_addr, sizeof(dest_addr));
-    if (err != 0) {
+    if (err != 0)
+    {
         ESP_LOGE(TAG, "Socket unable to connect: errno %d", errno);
         close(sock);
         return -1;
     }
-    
+
     ESP_LOGI(TAG, "Successfully connected to server");
     return sock;
 }
 
-static int send_message(int sock, const char *message) {
+static int send_message(int sock, const char *message)
+{
     int err = send(sock, message, strlen(message), 0);
-    if (err < 0) {
+    if (err < 0)
+    {
         ESP_LOGE(TAG, "Error occurred during sending: errno %d", errno);
         return -1;
     }
     return err;
 }
 
-static int receive_response(int sock, char *buffer, size_t buffer_size) {
+static int receive_response(int sock, char *buffer, size_t buffer_size)
+{
     int len = recv(sock, buffer, buffer_size - 1, 0);
-    
-    if (len < 0) {
+
+    if (len < 0)
+    {
         ESP_LOGE(TAG, "recv failed: errno %d", errno);
         return -1;
-    } else if (len == 0) {
+    }
+    else if (len == 0)
+    {
         ESP_LOGW(TAG, "Connection closed by server");
         return 0;
-    } else {
+    }
+    else
+    {
         buffer[len] = '\0'; // Null-terminate the received data
         ESP_LOGI(TAG, "Received %d bytes: %s", len, buffer);
         return len;
@@ -170,7 +174,7 @@ static esp_err_t i2c_master_init(void)
 
 static esp_err_t mpu6886_register_read(uint8_t reg_addr, uint8_t *data, size_t len)
 {
-    return i2c_master_write_read_device(I2C_MASTER_NUM, MPU6886_SENSOR_ADDR, &reg_addr, 1, data, len, 10*I2C_MASTER_TIMEOUT_MS / portTICK_PERIOD_MS);
+    return i2c_master_write_read_device(I2C_MASTER_NUM, MPU6886_SENSOR_ADDR, &reg_addr, 1, data, len, 10 * I2C_MASTER_TIMEOUT_MS / portTICK_PERIOD_MS);
 }
 
 static esp_err_t mpu6886_register_write_byte(uint8_t reg_addr, uint8_t data)
@@ -178,7 +182,7 @@ static esp_err_t mpu6886_register_write_byte(uint8_t reg_addr, uint8_t data)
     int ret;
     uint8_t write_buf[2] = {reg_addr, data};
 
-    ret = i2c_master_write_to_device(I2C_MASTER_NUM, MPU6886_SENSOR_ADDR, write_buf, sizeof(write_buf), 10*I2C_MASTER_TIMEOUT_MS / portTICK_PERIOD_MS);
+    ret = i2c_master_write_to_device(I2C_MASTER_NUM, MPU6886_SENSOR_ADDR, write_buf, sizeof(write_buf), 10 * I2C_MASTER_TIMEOUT_MS / portTICK_PERIOD_MS);
 
     return ret;
 }
@@ -211,8 +215,7 @@ static void init_mpu6886(void)
     vTaskDelay(10 / portTICK_PERIOD_MS);
 }
 
-
-static void getAccelAdc(int16_t* ax, int16_t* ay, int16_t* az)
+static void getAccelAdc(int16_t *ax, int16_t *ay, int16_t *az)
 {
     uint8_t buf[6];
     mpu6886_register_read(MPU6886_ACCEL_XOUT_REG_ADDR, buf, 6);
@@ -222,7 +225,7 @@ static void getAccelAdc(int16_t* ax, int16_t* ay, int16_t* az)
     *az = ((int16_t)buf[4] << 8) | buf[5];
 }
 
-static void getAccelData(float* ax, float* ay, float* az)
+static void getAccelData(float *ax, float *ay, float *az)
 {
     int16_t accX = 0;
     int16_t accY = 0;
@@ -235,59 +238,81 @@ static void getAccelData(float* ax, float* ay, float* az)
     *az = (float)accZ * aRes;
 }
 
-bool is_gravity(float abs_value) {
+bool is_gravity(float abs_value)
+{
     return abs_value > 0.8 && abs_value < 1.2;
 }
 
-device_orientation_t detect_orientation(float ax, float ay, float az) {
+device_orientation_t detect_orientation(float ax, float ay, float az)
+{
     // Take absolute values to simplify comparisons
     float abs_ax = fabsf(ax);
     float abs_ay = fabsf(ay);
     float abs_az = fabsf(az);
-    
+
     // Find which axis has the dominant acceleration
-    if (abs_ax > abs_ay && abs_ax > abs_az) {        // X axis is dominant
-        if (is_gravity(abs_ax)) {
+    if (abs_ax > abs_ay && abs_ax > abs_az)
+    { // X axis is dominant
+        if (is_gravity(abs_ax))
+        {
             return (ax > 0) ? ORIENTATION_POSITIVE_X : ORIENTATION_NEGATIVE_X;
         }
-    } 
-    else if (abs_ay > abs_ax && abs_ay > abs_az) {        // Y axis is dominant
-        if (is_gravity(abs_ay)) {
+    }
+    else if (abs_ay > abs_ax && abs_ay > abs_az)
+    { // Y axis is dominant
+        if (is_gravity(abs_ay))
+        {
             return (ay > 0) ? ORIENTATION_POSITIVE_Y : ORIENTATION_NEGATIVE_Y;
         }
-    } 
-    else if (abs_az > abs_ax && abs_az > abs_ay) {        // Z axis is dominant
-        if (is_gravity(abs_az)) {
+    }
+    else if (abs_az > abs_ax && abs_az > abs_ay)
+    { // Z axis is dominant
+        if (is_gravity(abs_az))
+        {
             return (az > 0) ? ORIENTATION_POSITIVE_Z : ORIENTATION_NEGATIVE_Z;
         }
     }
-    
+
     // If no clear dominant axis or values not in threshold
     return ORIENTATION_UNDEFINED;
 }
 
+static int send_orientation(int sock, device_orientation_t current_orientation)
+{
+    snprintf(tx_buffer, MAX_TX_SIZE, "%s", orientation_names[current_orientation]);
 
-static int task_one(int sock) {
-    vTaskDelay(pdMS_TO_TICKS(1000)); // 1 second delay
-    int random_num = esp_random();
-    
-    /* Use snprintf instead of itoa for safer, more portable conversion */
-    snprintf(tx_buffer, MAX_TX_SIZE, "%d", random_num);
-
-    if(send_message(sock, tx_buffer) < 0) {
+    if (send_message(sock, tx_buffer) < 0)
+    {
         return -1;
     }
-    
     return receive_response(sock, rx_buffer, MAX_RX_SIZE);
 }
 
-static int task_two(int sock) {
+static int task_one(int sock)
+{
+    vTaskDelay(pdMS_TO_TICKS(1000)); // 1 second delay
+    int random_num = esp_random();
+
+    /* Use snprintf instead of itoa for safer, more portable conversion */
+    snprintf(tx_buffer, MAX_TX_SIZE, "%d", random_num);
+
+    if (send_message(sock, tx_buffer) < 0)
+    {
+        return -1;
+    }
+
+    return receive_response(sock, rx_buffer, MAX_RX_SIZE);
+}
+
+static int task_two(int sock)
+{
     static int64_t last_timestamp = 0;
     float ax, ay, az;
     int64_t timestamp;
     int diff;
 
-    if (last_timestamp == 0) {
+    if (last_timestamp == 0)
+    {
         last_timestamp = esp_timer_get_time();
     }
 
@@ -302,43 +327,61 @@ static int task_two(int sock) {
     return 1;
 }
 
-static int task_three(int sock) {
-    static int64_t last_timestamp = 0;
-    
+static int task_three(int sock)
+{
+    static TickType_t last_wake_time = 0;
+
     int64_t timestamp;
     device_orientation_t current_orientation;
     float ax, ay, az;
-    int diff;
 
-    if (last_timestamp == 0) {
-        last_timestamp = esp_timer_get_time();
+    if (last_wake_time == 0)
+    {
+        last_wake_time = xTaskGetTickCount();
     }
 
-    timestamp = esp_timer_get_time(); // Get current time in microseconds
-    diff = (timestamp - last_timestamp) / 1000;
-    int to_wait = 100 - diff; // .1 second delay
-    if (to_wait < 0) {
-        ESP_LOGW(TAG, "took longer than 100 ms: %d", to_wait);
-    } else {
-        // ESP_LOGI(TAG, "to wait: %d", to_wait);
-        vTaskDelay(pdMS_TO_TICKS(to_wait)); 
-    }
-    last_timestamp = timestamp;
+    vTaskDelayUntil(&last_wake_time, pdMS_TO_TICKS(100));
+    timestamp = esp_timer_get_time() / 1000;
 
     getAccelData(&ax, &ay, &az);
-    ESP_LOGI(TAG, "acceleration data: %f %f %f, timestamp: %lld, timediff: %d ms", ax, ay, az, timestamp, diff);
+    ESP_LOGI(TAG, "acceleration data: %f %f %f, timestamp: %lld", ax, ay, az, timestamp);
 
     current_orientation = detect_orientation(ax, ay, az);
-    snprintf(tx_buffer, MAX_TX_SIZE, "%s", orientation_names[current_orientation]);
-
-    if(send_message(sock, tx_buffer) < 0) {
-        return -1;
-    }
-    return receive_response(sock, rx_buffer, MAX_RX_SIZE);
+    return send_orientation(sock, current_orientation);
 }
 
+static int task_four(int sock)
+{
+    static TickType_t last_wake_time = 0;
+    static device_orientation_t last_orientation = ORIENTATION_UNDEFINED;
 
-void tcp_client(void) {
+    int64_t timestamp;
+    device_orientation_t current_orientation;
+    float ax, ay, az;
+
+    if (last_wake_time == 0)
+    {
+        last_wake_time = xTaskGetTickCount();
+    }
+
+    // delay exactly the right amount, and update last_wake_time
+    vTaskDelayUntil(&last_wake_time, pdMS_TO_TICKS(100));
+    timestamp = esp_timer_get_time() / 1000;
+
+    getAccelData(&ax, &ay, &az);
+    ESP_LOGI(TAG, "acceleration data: %f %f %f, timestamp: %lld", ax, ay, az, timestamp);
+
+    current_orientation = detect_orientation(ax, ay, az);
+    if (current_orientation != last_orientation)
+    {
+        last_orientation = current_orientation;
+        return send_orientation(sock, current_orientation);
+    }
+    return 0;
+}
+
+void tcp_client(void)
+{
     char host_ip[] = HOST_IP_ADDR;
     int sock = -1;
     bool connected = false;
@@ -349,30 +392,37 @@ void tcp_client(void) {
 
     init_mpu6886();
     ESP_LOGI(TAG, "MPU6866 intialized successfully");
-    
+
     ESP_LOGI(TAG, "TCP client task starting...");
-    while (1) {
-        if (!connected) {
+    while (1)
+    {
+        if (!connected)
+        {
             sock = connect_to_server(host_ip, PORT);
-            if (sock >= 0) {
+            if (sock >= 0)
+            {
                 connected = true;
-            } else {
+            }
+            else
+            {
                 vTaskDelay(pdMS_TO_TICKS(RECONNECT_DELAY_MS)); // Wait before retrying
                 continue;
             }
         }
 
-        int task_res = task_three(sock);
-        if (task_res < 0) {
+        int task_res = task_four(sock);
+        if (task_res < 0)
+        {
             connected = false;
             close(sock);
             continue;
-        } 
+        }
     }
-    
+
     /* This code is not reached due to the infinite loop,
        but included for completeness */
-    if (sock != -1) {
+    if (sock != -1)
+    {
         ESP_LOGI(TAG, "Shutting down socket...");
         shutdown(sock, 0);
         close(sock);
