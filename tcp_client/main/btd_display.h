5bbc4af45f41be2639b25ff9ac09cf479bcf3c1e
macgoral <macgoral@outlook.com>
2025-06-21 22:51:59 +0200

    added time and break display

--- tcp_client/main/btd_display.h
+++ tcp_client/main/btd_display.h
@@ -3,3 +3,6 @@ void setup_display(void);
 void clear_display(void);
 void display_battery_percentage(int percentage);
 void display_qr_code(void);
+void display_time(int sec);
+void display_break_msg(void);
+void display_break_bar(void);
