71ee151794ef64827b397910c39bc0ad599574a7
macgoral <macgoral@outlook.com>
2025-06-25 06:08:17 +0200

    display funct. extended for working mode & qr codes deleted

--- tcp_client/main/btd_display.h
+++ tcp_client/main/btd_display.h
@@ -2,7 +2,10 @@
 void setup_display(void);
 void clear_display(void);
 void display_battery_percentage(int percentage);
-void display_qr_code(void);
+void display_wifi_code(void);
+void display_link_code(void);
 void display_time(int sec);
 void display_break_msg(void);
 void display_break_bar(void);
+void display_working_msg(void);
+void display_working_bar(void);
